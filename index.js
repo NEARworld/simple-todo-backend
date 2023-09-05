223330a74a40ad9e3173e2d5bf7ca5443d15b5af
nearworld <artemismars2@outlook.com>
2023-09-05 22:37:34 +0900

    feat: cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ dotenv.config();
 
 const express = require("express");
 const connection = require("./db");
+const cors = require("cors");
 const app = express();
 const PORT = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
